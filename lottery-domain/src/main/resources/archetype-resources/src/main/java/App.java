718a306f58bed3549f4fce621958e05b5d07764f
un1ink <un1ink@qq.com>
2023-03-20 19:04:59 +0800

    un1ink, chore: project Init.

---
+++ lottery-domain/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.un1ink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
