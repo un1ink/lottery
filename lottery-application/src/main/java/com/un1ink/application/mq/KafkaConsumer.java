8ed10249adb328caff53a4a63dde8fb4924368ba
un1ink <un1ink@qq.com>
2023-04-02 22:43:10 +0800

    un1ink, feat: 初次引入kafka

---
+++ lottery-application/src/main/java/com/un1ink/application/mq/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.un1ink.application.mq;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @description:
+ * @author：un1ink
+ * @date: 2023/4/2
+ */
+
+@Component
+public class KafkaConsumer {
+    private final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP)
+    public void TopicTest(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional<?> message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            logger.info("topic_test 消费了： Topic:" + topic + ",Message:" + msg);
+            ack.acknowledge();
+
+        }
+    }
+}
--- lottery-interfaces/src/test/java/com/un1ink/interfaces/LotteryActivityBoothTest.java
+++ lottery-interfaces/src/test/java/com/un1ink/interfaces/LotteryActivityBoothTest.java
@@ -30,9 +30,19 @@ public class LotteryActivityBoothTest {
     private ILotteryActivityBooth lotteryActivityBooth;
 
     @Test
-    public void test_doDraw() {
+
+    public void test(){
+        int epoch = 1;
+        for(int i = 0; i < epoch; i++){
+            String uId = "un1ink_hast_"+i;
+            test_doDraw(uId);
+            test_doQuantificationDraw(uId);
+        }
+    }
+//    @Test
+    public void test_doDraw(String uId) {
         DrawReq drawReq = new DrawReq();
-        drawReq.setUId("un1ink");
+        drawReq.setUId(uId);
         drawReq.setActivityId(100001L);
         DrawRes drawRes = lotteryActivityBooth.doDraw(drawReq);
         logger.info("请求参数：{}", JSON.toJSONString(drawReq));
@@ -40,10 +50,11 @@ public class LotteryActivityBoothTest {
 
     }
 
-    @Test
-    public void test_doQuantificationDraw() {
+//    @Test
+    public void test_doQuantificationDraw(String uId) {
         QuantificationDrawReq req = new QuantificationDrawReq();
-        req.setUId("un1ink_0405");
+        req.setUId(uId
+        );
         req.setTreeId(2110081902L);
         req.setValMap(new HashMap<String, Object>() {{
             put("gender", "man");
@@ -53,7 +64,6 @@ public class LotteryActivityBoothTest {
         DrawRes drawRes = lotteryActivityBooth.doQuantificationDraw(req);
         logger.info("请求参数：{}", JSON.toJSONString(req));
         logger.info("测试结果：{}", JSON.toJSONString(drawRes));
-
     }
 
 }
